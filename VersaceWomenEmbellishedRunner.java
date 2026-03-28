3b18cdec57fe4c602409c38262cb8472dc2cc821
keerthana08556 <keerthana.nagaraj085@gamil.com>
2026-03-28 12:57:56 +0530

    task 28-03-2026

---
+++ VersaceWomenEmbellishedRunner.java
@@ -0,0 +1,10 @@
+class VersaceWomenEmbellishedRunner {
+
+    public static void main(String[] args) {
+
+        // Creating object of another class
+        VersaceWomenEmbellished obj = new VersaceWomenEmbellished();
+
+        System.out.println("Display: " + obj.display());
+    }
+}
